fc2c0f786036a83208692274473ee36bc8510d6e
David Padilla <david@crowdint.com>
2012-11-11 20:20:22 -0600

    TabExample: First Controller layout

--- 4-TabExample/app/controllers/first_controller.rb
+++ 4-TabExample/app/controllers/first_controller.rb
@@ -1,5 +1,14 @@
 class FirstController < UIViewController
   def viewDidLoad
     self.view.backgroundColor = UIColor.whiteColor
+
+    label               = UILabel.alloc.initWithFrame(self.view.bounds)
+    label.text          = "1"
+    label.font          = UIFont.systemFontOfSize(300)
+    label.textAlignment = NSTextAlignmentCenter
+
+    self.view.addSubview label
+
+    super
   end
 end

02da06fced8274aa389c2225f4009b4393af2bb7
David Padilla <david@crowdint.com>
2013-06-25 11:38:21 -0500

    BatteryMonitor: Made the window variable an instance variable or else it just doesn't work

--- 3-BatteryMonitor/app/app_delegate.rb
+++ 3-BatteryMonitor/app/app_delegate.rb
@@ -6,10 +6,10 @@ class AppDelegate
     monitor_battery = false
 
     self.main_view_controller = MainViewController.alloc.initWithNibName(nil, bundle:nil)
-    window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
 
-    window.rootViewController = self.main_view_controller
-    window.makeKeyAndVisible
+    @window.rootViewController = self.main_view_controller
+    @window.makeKeyAndVisible
 
     true
   end

1eb6a1dc6ea156a0a599d460b198a11aa37fd7e7
David Padilla <david@crowdint.com>
2013-06-25 11:41:28 -0500

    Prototype: Made the window variable an instance variable

--- 6-Prototype/app/app_delegate.rb
+++ 6-Prototype/app/app_delegate.rb
@@ -1,11 +1,11 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
-    window.makeKeyAndVisible
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.makeKeyAndVisible
 
     controller = PrototypeViewController.alloc.initWithNibName(nil, bundle: nil)
 
-    window.rootViewController = controller
+    @window.rootViewController = controller
 
     true
   end
